e056adcdfef22a2a7e5c1c06d5ca908d3087e08d
Igor Streshnev <igor.streshnev2019@gmail.com>
2024-04-19 19:52:40 +0300

    dependencies managment

---
+++ feature/auth/src/main/java/com/monke/auth/StartFragment.java
@@ -0,0 +1,36 @@
+package com.monke.auth;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class StartFragment extends Fragment {
+
+    private StartViewModel mViewModel;
+
+    public static StartFragment newInstance() {
+        return new StartFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_start, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(StartViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ feature/auth/src/main/java/com/monke/auth/StartViewModel.java
@@ -0,0 +1,7 @@
+package com.monke.auth;
+
+import androidx.lifecycle.ViewModel;
+
+public class StartViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
