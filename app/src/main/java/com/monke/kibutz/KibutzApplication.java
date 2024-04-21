ef73989e826d9bad1604d5acf221eabf7f18c268
Igor Streshnev <igor.streshnev2019@gmail.com>
2024-04-21 19:41:30 +0300

    mapkit dependency

---
+++ app/src/main/java/com/monke/kibutz/KibutzApplication.java
@@ -0,0 +1,14 @@
+package com.monke.kibutz;
+
+import android.app.Application;
+
+import com.yandex.mapkit.MapKitFactory;
+
+public class KibutzApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        MapKitFactory.setApiKey("43cfc245-e9c5-42d5-b13b-1df20b465d9f");
+    }
+}
