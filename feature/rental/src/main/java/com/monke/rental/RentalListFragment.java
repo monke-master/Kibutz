0f86a1993c2b17bd7fd92de2162270021c487a63
Igor Streshnev <igor.streshnev2019@gmail.com>
2024-04-22 23:19:20 +0300

    RentalFragment

---
+++ feature/rental/src/main/java/com/monke/rental/RentalListFragment.java
@@ -0,0 +1,36 @@
+package com.monke.rental;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class RentalListFragment extends Fragment {
+
+    private RentalListViewModel mViewModel;
+
+    public static RentalListFragment newInstance() {
+        return new RentalListFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_rental_list, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(RentalListViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
