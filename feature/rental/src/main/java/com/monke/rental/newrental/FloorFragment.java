837b867a76d3499b60b8666a5ca7eccb5d0cd9be
Igor Streshnev <igor.streshnev2019@gmail.com>
2024-04-23 14:02:49 +0300

    Floor Fragment

---
+++ feature/rental/src/main/java/com/monke/rental/newrental/FloorFragment.java
@@ -0,0 +1,38 @@
+package com.monke.rental.newrental;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.monke.rental.R;
+
+public class FloorFragment extends Fragment {
+
+    private FloorViewModel mViewModel;
+
+    public static FloorFragment newInstance() {
+        return new FloorFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_floor, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(FloorViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ feature/rental/src/main/java/com/monke/rental/newrental/FloorViewModel.java
@@ -0,0 +1,7 @@
+package com.monke.rental.newrental;
+
+import androidx.lifecycle.ViewModel;
+
+public class FloorViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
