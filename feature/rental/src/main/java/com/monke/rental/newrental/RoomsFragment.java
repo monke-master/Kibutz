941e983d75637e59a5f233dba0ffd1fb0bf7a7c4
Igor Streshnev <igor.streshnev2019@gmail.com>
2024-04-23 14:58:42 +0300

    fuck supchiks

---
+++ feature/rental/src/main/java/com/monke/rental/newrental/RoomsFragment.java
@@ -0,0 +1,38 @@
+package com.monke.rental.newrental;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.monke.rental.R;
+
+public class RoomsFragment extends Fragment {
+
+    private RoomsViewModel mViewModel;
+
+    public static RoomsFragment newInstance() {
+        return new RoomsFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_rooms, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(RoomsViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ feature/rental/src/main/java/com/monke/rental/newrental/RoomsViewModel.java
@@ -0,0 +1,7 @@
+package com.monke.rental.newrental;
+
+import androidx.lifecycle.ViewModel;
+
+public class RoomsViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
