694299cff64052581cdb04f1646291e7ce2dcab8
Igor Streshnev <igor.streshnev2019@gmail.com>
2024-04-24 15:33:24 +0300

    Profile Fragment

---
+++ feature/profile/src/main/java/com/example/profile/EditProfileFragment.java
@@ -0,0 +1,36 @@
+package com.example.profile;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class EditProfileFragment extends Fragment {
+
+    private EditProfileViewModel mViewModel;
+
+    public static EditProfileFragment newInstance() {
+        return new EditProfileFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_edit_profile, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(EditProfileViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ feature/profile/src/main/java/com/example/profile/EditProfileViewModel.java
@@ -0,0 +1,7 @@
+package com.example.profile;
+
+import androidx.lifecycle.ViewModel;
+
+public class EditProfileViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
