c66772f91428594dab8c18d822237701318ce753
Igor Streshnev <igor.streshnev2019@gmail.com>
2024-04-25 16:20:08 +0300

    auth nav graph

--- app/src/main/java/com/monke/kibutz/KibutzMainActivity.java
+++ app/src/main/java/com/monke/kibutz/KibutzMainActivity.java
@@ -4,12 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.monke.kibutz.databinding.ActivityMainBinding;
+
 public class KibutzMainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding mBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        mBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(mBinding.getRoot());
     }
 }
